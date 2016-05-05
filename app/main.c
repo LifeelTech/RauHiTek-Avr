35961d045420fbaacb3d7f62ff1de1f886560feb
ddhuy <ddhuy137@yahoo.com>
2016-05-06 01:06:39 +0700

    use <util/delay.h> instead

--- app/main.c
+++ app/main.c
@@ -8,7 +8,7 @@
 
 #include "stdlib.h"
 #include <Arduino.h>
-#include <delay.h>
+#include <util/delay.h>
 
 int
 main(void)
@@ -17,9 +17,9 @@ main(void)
 	while (1)
 	{
 		digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-		_delay_ms(1000);              // wait for a second
+		delay(1000);              // wait for a second
 		digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-		_delay_ms(1000);              // wait for a second
+		delay(1000);              // wait for a second
 	}
 	return 0;
 }
